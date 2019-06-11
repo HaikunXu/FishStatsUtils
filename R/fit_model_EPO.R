fc1b6d58a92c17aa7ed62bb75c8233a73bd553b1
Haikun Xu <hkxu@iattc.org>
2019-06-11 09:51:41 -0700

    recover default functions

--- R/fit_model_EPO.R
+++ R/fit_model_EPO.R
@@ -16,7 +16,6 @@
 #' @param optimize_args tagged list of optional arguments to pass to \code{TMBhelper::Optimize}
 #' @param model_args tagged list of optional arguments to pass to \code{VAST::make_model}
 #' @param run_model Boolean indicating whether to run the model or simply return the inputs and built TMB object
-#' @param test_fit Boolean indicating whether to apply \code{VAST::check_fit} before calculating standard errors, to test for parameters hitting bounds etc; defaults to TRUE
 #' @param ... additional parameters to pass to \code{VAST::make_data}
 #'
 #' @return Returns a tagged list of internal objects, the TMB object, and slot \code{parameter_estimates} containing the MLE estimates
@@ -50,12 +49,12 @@
 #' }
 #'
 #' @export
-fit_model = function( settings, Lat_i, Lon_i, t_iz, c_iz, b_i, a_i,
-                      v_i=rep(0,length(b_i)), working_dir=paste0(getwd(),"/"),
-                      Xconfig_zcp=NULL, X_gtp=NULL, X_itp=NULL, Q_ik=NULL, newtonsteps=1,
-                      extrapolation_args=list(), spatial_args=list(), optimize_args=list(), model_args=list(),
-                      silent=TRUE, run_model=TRUE, test_fit=TRUE, ... ){
-  
+fit_model = function( settings, zone, Data, t_iz, c_iz, b_i, a_i,
+  v_i=rep(0,length(b_i)), working_dir=paste0(getwd(),"/"),
+  Xconfig_zcp=NULL, X_gtp=NULL, X_itp=NULL, Q_ik=NULL, newtonsteps=1,
+  extrapolation_args=list(), spatial_args=list(), optimize_args=list(), model_args=list(),
+  silent=TRUE, run_model=TRUE, ... ){
+
   # Local function -- combine two lists
   combine_lists = function( default, input ){
     output = default
@@ -68,86 +67,103 @@ fit_model = function( settings, Lat_i, Lon_i, t_iz, c_iz, b_i, a_i,
     }
     return( output )
   }
+
+  Lat_i <- Data_Geostat[,'Lat']
+  Lon_i <- Data_Geostat[,'Lon']
   
   # Assemble inputs
   data_frame = data.frame( "Lat_i"=Lat_i, "Lon_i"=Lon_i, "a_i"=a_i, "v_i"=v_i, "b_i"=b_i )
   # Decide which years to plot
   year_labels = seq( min(t_iz), max(t_iz) )
   years_to_plot = which( unique(t_iz) %in% sort(unique(t_iz)))
-  
+
   # Save record
   dir.create(working_dir, showWarnings=FALSE, recursive=TRUE)
-  #save( settings, file=file.path(working_dir,"Record.RData"))
-  capture.output( settings, file=file.path(working_dir,"settings.txt"))
-  
+  save( settings, file=file.path(working_dir,"Record.RData"))
+  capture.output( settings, file=file.path(working_dir,"Record.txt"))
+
   # Build extrapolation grid
   message("\n### Making extrapolation-grid")
-  extrapolation_args = combine_lists( input=extrapolation_args, default=list(Region=settings$Region, strata.limits=settings$strata.limits, zone=settings$zone) )
-  extrapolation_list = do.call( what=make_extrapolation_info, args=extrapolation_args )
+
+  lat_North <- Data_Geostat$Lat > 0
+  Data_Geostat_North <- Data_Geostat[lat_North,]
+  Extrapolation_List_North = make_extrapolation_info(Region=Region, zone = zone, strata.limits=strata.limits, observations_LL=Data_Geostat_North[,c('Lat','Lon')], grid_dim_km=c(25,25) )
   
-  # Build information regarding spatial location and correlation
-  message("\n### Making spatial information")
-  spatial_args = combine_lists( input=spatial_args, default=list(grid_size_km=settings$grid_size_km, n_x=settings$n_x, Method=settings$Method, Lon_i=Lon_i, Lat_i=Lat_i,
-                                                                 Extrapolation_List=extrapolation_list, DirPath=working_dir, Save_Results=TRUE, fine_scale=settings$fine_scale) )
-  #spatial_list = make_spatial_info( grid_size_km=settings$grid_size_km, n_x=settings$n_x, Method=settings$Method, Lon_i=Lon_i, Lat_i=Lat_i,
-  #  Extrapolation_List=extrapolation_list, DirPath=working_dir, Save_Results=TRUE, fine_scale=settings$fine_scale )
-  spatial_list = do.call( what=make_spatial_info, args=spatial_args )
+  lat_South <- Data_Geostat$Lat < 0
+  Data_Geostat_South <- Data_Geostat[lat_South,]
+  Extrapolation_List_South = make_extrapolation_info(Region=Region, zone = zone, strata.limits=strata.limits, observations_LL=Data_Geostat_South[,c('Lat','Lon')], grid_dim_km=c(25,25) )
   
+  Extrapolation_List_South$Data_Extrap$N_km <- Extrapolation_List_South$Data_Extrap$N_km - 10000
+  
+  a_el <- rbind(Extrapolation_List_North$a_el,Extrapolation_List_South$a_el)
+  Data_Extrap <- rbind(Extrapolation_List_North$Data_Extrap,Extrapolation_List_South$Data_Extrap)
+  zone <- Extrapolation_List_North$zone
+  flip_around_dateline <- Extrapolation_List_North$flip_around_dateline
+  Area_km2_x <- c(Extrapolation_List_North$Area_km2_x,Extrapolation_List_South$Area_km2_x)
+  
+  extrapolation_list <- list("a_el" = a_el, "Data_Extrap" = Data_Extrap, "zone" = zone,
+                             "flip_around_dateline" = flip_around_dateline,
+                             "Area_km2_x" = Area_km2_x)
+
+  spatial_list = make_spatial_info_EPO( grid_size_km=grid_size_km, n_x=n_x, Method=Method, Lon=Data_Geostat[,'Lon'], Lat=Data_Geostat[,'Lat'], Extrapolation_List=extrapolation_list, DirPath=DateFile, Save_Results=FALSE )
+  plot_data(Extrapolation_List=extrapolation_list, Spatial_List=spatial_list, Data_Geostat=Data_Geostat )
+  
+
   # Build data
   message("\n### Making data object") # VAST::
   data_list = VAST::make_data("Version"=settings$Version, "FieldConfig"=settings$FieldConfig, "OverdispersionConfig"=settings$OverdispersionConfig,
-                              "RhoConfig"=settings$RhoConfig, "ObsModel"=settings$ObsModel, "c_iz"=c_iz, "b_i"=b_i, "a_i"=a_i, "v_i"=v_i,
-                              "s_i"=spatial_list$knot_i-1, "t_iz"=t_iz, "spatial_list"=spatial_list, "Options"=settings$Options, "Aniso"=settings$use_anisotropy,
-                              Xconfig_zcp=Xconfig_zcp, X_gtp=X_gtp, X_itp=X_itp, Q_ik=Q_ik, ... )
-  
+    "RhoConfig"=settings$RhoConfig, "ObsModel"=settings$ObsModel, "c_iz"=c_iz, "b_i"=b_i, "a_i"=a_i, "v_i"=v_i,
+    "s_i"=spatial_list$knot_i-1, "t_iz"=t_iz, "spatial_list"=spatial_list, "Options"=settings$Options, "Aniso"=settings$use_anisotropy,
+    Xconfig_zcp=Xconfig_zcp, X_gtp=X_gtp, X_itp=X_itp, Q_ik=Q_ik, ... )
+
   # Build object
   message("\n### Making TMB object")
   model_args = combine_lists( input=model_args, default=list("TmbData"=data_list, "RunDir"=working_dir, "Version"=settings$Version,
-                                                             "RhoConfig"=settings$RhoConfig, "loc_x"=spatial_list$loc_x, "Method"=spatial_list$Method) )
+    "RhoConfig"=settings$RhoConfig, "loc_x"=spatial_list$loc_x, "Method"=spatial_list$Method) )
   tmb_list = do.call( what=VAST::make_model, args=model_args )  # VAST::
   if(silent==TRUE) tmb_list$Obj$env$beSilent()
-  
+
   # Run the model or optionally don't
   if( run_model==FALSE ){
     # Build and output
     Return = list("data_frame"=data_frame, "extrapolation_list"=extrapolation_list, "spatial_list"=spatial_list,
-                  "data_list"=data_list, "tmb_list"=tmb_list, "year_labels"=year_labels, "years_to_plot"=years_to_plot,
-                  "settings"=settings, "extrapolation_args"=extrapolation_args, "model_args"=model_args)
+      "data_list"=data_list, "tmb_list"=tmb_list, "year_labels"=year_labels, "years_to_plot"=years_to_plot,
+      "settings"=settings, "extrapolation_args"=extrapolation_args, "model_args"=model_args)
     return(Return)
   }
-  
+
   # Optimize object
   message("\n### Estimating parameters")
   optimize_args_phase1 = combine_lists( default=optimize_args, input=list(obj=tmb_list$Obj, lower=tmb_list$Lower, upper=tmb_list$Upper,
-                                                                          savedir=NULL, getsd=FALSE, newtonsteps=0, bias.correct=FALSE, quiet=TRUE,
-                                                                          control=list(eval.max=10000,iter.max=10000,trace=1), loopnum=2) )
+    savedir=working_dir, getsd=FALSE, newtonsteps=0, bias.correct=FALSE, quiet=TRUE,
+    control=list(eval.max=10000,iter.max=10000,trace=1), loopnum=2) )
   parameter_estimates = do.call( what=TMBhelper::fit_tmb, args=optimize_args_phase1 )
-  
+
   # Check fit of model (i.e., evidence of non-convergence based on bounds, approaching zero, etc)
-  if(exists("check_fit") & test_fit==TRUE ){
+  if(exists("check_fit")){
     problem_found = VAST::check_fit( parameter_estimates )
     if( problem_found==TRUE ){
       message("\n")
       stop("Please change model structure to avoid problems with parameter estimates and then re-try\n", call.=FALSE)
     }
   }
-  
+
   # Restart estimates after checking parameters
   optimize_args_phase2 = combine_lists( input=optimize_args, default=list(obj=tmb_list$Obj, lower=tmb_list$Lower, upper=tmb_list$Upper,
-                                                                          savedir=working_dir, bias.correct=settings$bias.correct, newtonsteps=newtonsteps,
-                                                                          bias.correct.control=list(sd=FALSE, split=NULL, nsplit=1, vars_to_correct=settings$vars_to_correct),
-                                                                          control=list(eval.max=10000,iter.max=10000,trace=1), loopnum=1) )
+    savedir=working_dir, bias.correct=settings$bias.correct, newtonsteps=newtonsteps,
+    bias.correct.control=list(sd=FALSE, split=NULL, nsplit=1, vars_to_correct=settings$vars_to_correct),
+    control=list(eval.max=10000,iter.max=10000,trace=1), loopnum=1) )
   optimize_args_phase2 = combine_lists( input=list(startpar=parameter_estimates$par), default=optimize_args_phase2 )
   parameter_estimates = do.call( what=TMBhelper::fit_tmb, args=optimize_args_phase2 )
-  
+
   # Extract standard outputs
   Report = tmb_list$Obj$report()
   ParHat = tmb_list$Obj$env$parList( parameter_estimates$par )
-  
+
   # Build and output
   Return = list("data_frame"=data_frame, "extrapolation_list"=extrapolation_list, "spatial_list"=spatial_list,
-                "data_list"=data_list, "tmb_list"=tmb_list, "parameter_estimates"=parameter_estimates, "Report"=Report,
-                "ParHat"=ParHat, "year_labels"=year_labels, "years_to_plot"=years_to_plot, "settings"=settings,
-                "extrapolation_args"=extrapolation_args, "model_args"=model_args, "optimize_args"=optimize_args)
+    "data_list"=data_list, "tmb_list"=tmb_list, "parameter_estimates"=parameter_estimates, "Report"=Report,
+    "ParHat"=ParHat, "year_labels"=year_labels, "years_to_plot"=years_to_plot, "settings"=settings,
+    "extrapolation_args"=extrapolation_args, "model_args"=model_args, "optimize_args"=optimize_args)
   return( Return )
 }
--- R/make_spatial_info_EPO.R
+++ R/make_spatial_info_EPO.R
@@ -7,18 +7,17 @@
 #'
 #' \code{LON_intensity} and \code{LAT_intensity} allow users to specify locations that are used by the k-means algorithm to determine the location of knots, where e.g. users can either hard-code the desired knot locations via these inputs (using \code{n_x} greater than this number of locations), or use the extrapolation-grid to ensure that knots are located proportional to that grid.
 #'
-#' @param n_x the number of vertices in the SPDE mesh (determines the spatial resolution when Method="Mesh")
-#' @param Lon_i Longitude for each sample
-#' @param Lat_i Latitude for each sample
-#' @param LON_intensity Longitude for each location to use during k-means algorithm to decide upon the location of knots; defaults to location for sample \code{Lon_i}
-#' @param LAT_intensity Latitude for each location to use during k-means algorithm to decide upon the location of knots; defaults to location for sample \code{Lat_i}
-#' @param Method a character of either "Grid" or "Mesh" where "Grid" is a 2D AR1 process, and "Mesh" is the SPDE method with geometric anisotropy
+#' @param n_x, the number of vertices in the SPDE mesh (determines the spatial resolution when Method="Mesh")
+#' @param Lon_i, Longitude for each sample
+#' @param Lat_i, Latitude for each sample
+#' @param LON_intensity, Longitude for each location to use during k-means algorithm to decide upon the location of knots
+#' @param LAT_intensity, Latitude for each location to use during k-means algorithm to decide upon the location of knots
+#' @param Method, a character of either "Grid" or "Mesh" where "Grid" is a 2D AR1 process, and "Mesh" is the SPDE method with geometric anisotropy
 #' @param fine_scale a Boolean indicating whether to ignore (\code{fine_scale=FALSE}) or account for (\code{fine_scale=TRUE}) fine-scale spatial heterogeneity;  See details for more informatino
-#' @param Extrapolation_List the output from \code{Prepare_Extrapolation_Data_Fn}
-#' @param grid_size_km the distance between grid cells for the 2D AR1 grid (determines spatial resolution when Method="Grid") when not using \code{Method="Spherical_mesh"}
-#' @param grid_size_LL the distance between grid cells for the 2D AR1 grid (determines spatial resolution when Method="Grid") when using \code{Method="Spherical_mesh"}
-#' @param Network_sz_LL data frame with "parent_s", "child_s", "dist_s", "Lat", "Lon", default=NULL only needed with Method == "Stream_network"
-#' @param ... additional arguments passed to \code{INLA::inla.mesh.create}
+#' @param Extrapolation_List, the output from \code{Prepare_Extrapolation_Data_Fn}
+#' @param grid_size_km, the distance between grid cells for the 2D AR1 grid (determines spatial resolution when Method="Grid") when not using \code{Method="Spherical_mesh"}
+#' @param grid_size_LL, the distance between grid cells for the 2D AR1 grid (determines spatial resolution when Method="Grid") when using \code{Method="Spherical_mesh"}
+#' @param ..., additional arguments passed to \code{INLA::inla.mesh.create}
 #' @inheritParams Calc_Kmeans
 
 #' @return Tagged list containing objects for running a VAST model
@@ -36,26 +35,23 @@
 
 #' @export
 make_spatial_info = function( n_x, Lon_i, Lat_i, LON_intensity=Lon_i, LAT_intensity=Lat_i, Extrapolation_List, Method="Mesh",
-                              grid_size_km=50, grid_size_LL=1, fine_scale=FALSE, Network_sz_LL=NULL,
-                              iter.max=1000, randomseed=1, nstart=100, DirPath=paste0(getwd(),"/"), Save_Results=FALSE, ... ){
-  
+  grid_size_km=50, grid_size_LL=1, fine_scale=FALSE,
+  iter.max=1000, randomseed=1, nstart=100, DirPath=paste0(getwd(),"/"), Save_Results=FALSE, ... ){
+
   # Deprecated options
   if( Method=="Spherical_mesh" ){
     stop("Method=`Spherical_mesh` is not being maintained, but please write the package author if you need to explore this option")
   }
-  if( Method == "Stream_network" & fine_scale == TRUE ){
-    stop("Please use fine_scale=FALSE with stream network spatial model; feature fine_scale=TRUE not yet supported for stream network spatial model.")
-  }
-  
+
   # Convert to an Eastings-Northings coordinate system
   if( Method=="Spherical_mesh" ){
     loc_i = data.frame( 'Lon'=Lon_i, 'Lat'=Lat_i )
     # Bounds for 2D AR1 grid
     Grid_bounds = (grid_size_km/110) * apply(loc_e/(grid_size_km/110), MARGIN=2, FUN=function(vec){trunc(range(vec))+c(0,1)})
-    
+
     # Calculate k-means centroids
     Kmeans = Calc_Kmeans(n_x=n_x, loc_orig=loc_i[,c("Lon", "Lat")], randomseed=randomseed, ... )
-    
+
     # Calculate grid for 2D AR1 process
     loc_grid = expand.grid( 'Lon'=seq(Grid_bounds[1,1],Grid_bounds[2,1],by=grid_size_LL), 'Lat'=seq(Grid_bounds[1,2],Grid_bounds[2,2],by=grid_size_LL) )
     Which = sort(unique(RANN::nn2(data=loc_grid, query=Extrapolation_List$Data_Extrap[which(Extrapolation_List$Area_km2_x>0),c('Lon','Lat')], k=1)$nn.idx[,1]))
@@ -67,42 +63,52 @@ make_spatial_info = function( n_x, Lon_i, Lat_i, LON_intensity=Lon_i, LAT_intens
       # Locations for samples
       loc_i = Convert_LL_to_UTM_Fn( Lon=Lon_i, Lat=Lat_i, zone=Extrapolation_List$zone, flip_around_dateline=Extrapolation_List$flip_around_dateline )                                                         #$
       loc_i = cbind( 'E_km'=loc_i[,'X'], 'N_km'=loc_i[,'Y'])
+      
+      ind <- loc_i[,2]>6000
+      loc_i[ind,2] <- loc_i[ind,2] - 10000
+      
       # Locations for locations for knots
       loc_intensity = Convert_LL_to_UTM_Fn( Lon=LON_intensity, Lat=LAT_intensity, zone=Extrapolation_List$zone, flip_around_dateline=Extrapolation_List$flip_around_dateline )                                                         #$
       loc_intensity = cbind( 'E_km'=loc_intensity[,'X'], 'N_km'=loc_intensity[,'Y'])
+      
+      ind <- loc_intensity[,2]>6000
+      loc_intensity[ind,2] <- loc_intensity[ind,2] - 10000
+      
     }else{
       loc_i = Convert_LL_to_EastNorth_Fn( Lon=Lon_i, Lat=Lat_i, crs=Extrapolation_List$zone )
+      
+      ind <- loc_i[,2]>6000
+      loc_i[ind,2] <- loc_i[ind,2] - 10000
+      
       loc_intensity = Convert_LL_to_EastNorth_Fn( Lon=LON_intensity, Lat=LAT_intensity, crs=Extrapolation_List$zone )
+      
+      ind <- loc_intensity[,2]>6000
+      loc_intensity[ind,2] <- loc_intensity[ind,2] - 10000
     }
     # Bounds for 2D AR1 grid
     Grid_bounds = grid_size_km * apply(Extrapolation_List$Data_Extrap[,c('E_km','N_km')]/grid_size_km, MARGIN=2, FUN=function(vec){trunc(range(vec))+c(0,1)})
-    
+
     # Calculate k-means centroids
     Kmeans = Calc_Kmeans(n_x=n_x, loc_orig=loc_intensity[,c("E_km", "N_km")], randomseed=randomseed, nstart=nstart, DirPath=DirPath, Save_Results=Save_Results )
     NN_i = RANN::nn2( data=Kmeans[["centers"]], query=loc_i, k=1)$nn.idx[,1]
-    
+
     # Calculate grid for 2D AR1 process
     loc_grid = expand.grid( 'E_km'=seq(Grid_bounds[1,1],Grid_bounds[2,1],by=grid_size_km), 'N_km'=seq(Grid_bounds[1,2],Grid_bounds[2,2],by=grid_size_km) )
     Which = sort(unique(RANN::nn2(data=loc_grid, query=Extrapolation_List$Data_Extrap[which(Extrapolation_List$Area_km2_x>0),c('E_km','N_km')], k=1)$nn.idx[,1]))
     loc_grid = loc_grid[Which,]
     grid_num = RANN::nn2( data=loc_grid, query=loc_i, k=1)$nn.idx[,1]
   }
-  
+
   # Calc design matrix and areas
   if( Method=="Grid" ){
     knot_i = grid_num
     loc_x = loc_grid
   }
-  if( Method %in% c("Mesh","Spherical_mesh") ){
+  if( Method %in% c("Mesh","Spherical_mesh","Stream_network") ){
     knot_i = NN_i
     loc_x = Kmeans[["centers"]]
   }
-  if( Method == "Stream_network" ){
-    knot_i = Extrapolation_List$Data_Extrap[,"child_i"]
-    loc_x_recalc = Convert_LL_to_UTM_Fn( Lon=Network_sz_LL[,"Lon"], Lat=Network_sz_LL[,"Lat"], zone=Extrapolation_List$zone, flip_around_dateline=Extrapolation_List$flip_around_dateline )
-    loc_x = cbind( 'E_km'=loc_x_recalc[,"X"], 'N_km'=loc_x_recalc[,"Y"])
-  }
-  
+
   # Bookkeeping for extrapolation-grid
   if( fine_scale==FALSE ){
     loc_g = loc_x
@@ -110,18 +116,18 @@ make_spatial_info = function( n_x, Lon_i, Lat_i, LON_intensity=Lon_i, LAT_intens
   if( fine_scale==TRUE ){
     loc_g = Extrapolation_List$Data_Extrap[ which(Extrapolation_List$Area_km2_x>0), c('E_km','N_km') ]
   }
-  
+
   # Convert loc_x back to location in lat-long coordinates loc_x_LL
   # if zone=NA or NTULL, then it automatically detects appropriate zone
   #tmpUTM = cbind('PID'=1,'POS'=1:nrow(loc_x),'X'=loc_x[,'E_km'],'Y'=loc_x[,'N_km'])
   #attr(tmpUTM,"projection") = "UTM"
   #attr(tmpUTM,"zone") = Extrapolation_List$zone
   #loc_x_LL = PBSmapping::convUL(tmpUTM)                                                         #$
-  
+
   # Make mesh and info for anisotropy  SpatialDeltaGLMM::
   MeshList = Calc_Anisotropic_Mesh( Method=Method, loc_x=Kmeans$centers, loc_g=loc_g, loc_i=loc_i, Extrapolation_List=Extrapolation_List, fine_scale=fine_scale, ... )
   n_s = switch( tolower(Method), "mesh"=MeshList$anisotropic_spde$n.spde, "grid"=nrow(loc_x), "spherical_mesh"=MeshList$isotropic_spde$n.spde, "stream_network"=nrow(loc_x) )
-  
+
   # Make matrices for 2D AR1 process
   Dist_grid = dist(loc_grid, diag=TRUE, upper=TRUE)
   M0 = as( ifelse(as.matrix(Dist_grid)==0, 1, 0), "dgTMatrix" )
@@ -129,7 +135,7 @@ make_spatial_info = function( n_x, Lon_i, Lat_i, LON_intensity=Lon_i, LAT_intens
   M2 = as( ifelse(as.matrix(Dist_grid)==sqrt(2)*grid_size_km, 1, 0), "dgTMatrix" )
   if( Method=="Spherical_mesh" ) GridList = list("M0"=M0, "M1"=M1, "M2"=M2, "grid_size_km"=grid_size_LL)
   if( Method %in% c("Mesh","Grid","Stream_network") ) GridList = list("M0"=M0, "M1"=M1, "M2"=M2, "grid_size_km"=grid_size_km)
-  
+
   # Make projection matrices
   if( fine_scale==FALSE ){
     A_is = matrix(0, nrow=nrow(loc_i), ncol=n_s)
@@ -150,27 +156,20 @@ make_spatial_info = function( n_x, Lon_i, Lat_i, LON_intensity=Lon_i, LAT_intens
       # points( x=loc_i[which(Check_i!=3),'E_km'], y=loc_i[which(Check_i!=3),'N_km'], col="red" )
     }
   }
-  
+
   # Calculate areas
-  if( Method != "Stream_network" ){
-    PolygonList = Calc_Polygon_Areas_and_Polygons_Fn( loc_x=loc_x, Data_Extrap=Extrapolation_List[["Data_Extrap"]], a_el=Extrapolation_List[["a_el"]])
-    if( fine_scale==FALSE ){
-      a_gl = PolygonList[["a_xl"]]
-    }
-    if( fine_scale==TRUE ){
-      a_gl = as.matrix(Extrapolation_List[["a_el"]][ which(Extrapolation_List$Area_km2_x>0), ])
-    }
-  }else{
-    PolygonList = NULL
-    dist_inp = Network_sz_LL[,"dist_s"]
-    dist_inp[which(is.infinite(dist_inp))] <- 0
-    a_gl = matrix(dist_inp, nrow=n_x)
+  PolygonList = Calc_Polygon_Areas_and_Polygons_Fn( loc_x=loc_x, Data_Extrap=Extrapolation_List[["Data_Extrap"]], a_el=Extrapolation_List[["a_el"]])
+  if( fine_scale==FALSE ){
+    a_gl = PolygonList[["a_xl"]]
   }
-  
+  if( fine_scale==TRUE ){
+    a_gl = as.matrix(Extrapolation_List[["a_el"]][ which(Extrapolation_List$Area_km2_x>0), ])
+  }
+
   # Return
   Return = list( "fine_scale"=fine_scale, "A_is"=A_is, "A_gs"=A_gs, "n_x"=n_x, "n_s"=n_s, "n_g"=nrow(a_gl), "n_i"=nrow(loc_i),
-                 "MeshList"=MeshList, "GridList"=GridList, "a_gl"=a_gl, "a_xl"=a_gl, "Kmeans"=Kmeans, "knot_i"=knot_i,
-                 "loc_i"=as.matrix(loc_i), "loc_x"=as.matrix(loc_x), "loc_g"=as.matrix(loc_g),
-                 "Method"=Method, "PolygonList"=PolygonList, "NN_Extrap"=PolygonList$NN_Extrap )
+    "MeshList"=MeshList, "GridList"=GridList, "a_gl"=a_gl, "a_xl"=a_gl, "Kmeans"=Kmeans, "knot_i"=knot_i,
+    "loc_i"=as.matrix(loc_i), "loc_x"=as.matrix(loc_x), "loc_g"=as.matrix(loc_g),
+    "Method"=Method, "PolygonList"=PolygonList, "NN_Extrap"=PolygonList$NN_Extrap )
   return( Return )
 }
